2b902c86e380b84dec904584b8b73d85fe4dec15
hamayelq <52521315+hamayelq@users.noreply.github.com>
2020-01-27 17:50:46 -0500

    Update vga_display_logic.v

--- project_2.srcs/sources_1/new/vga_display_logic.v
+++ project_2.srcs/sources_1/new/vga_display_logic.v
@@ -20,18 +20,56 @@ module vga_display_logic (
 
     //need to make a decoder module. how do we make this work?
     //basically converting a and b input to seven seg
+    segment_decode(.switchChoice(PRESS_A), .SEG(SEG1));
+    segment_decode(.switchChoice(PRESS_B), .SEG(SEG2));
 
-    parameter ZEROES = 4'b0000;
-    parameter ONES   = 4'b1111;
+    parameter BLACK = 12'b0000_0000_0000;
+    parameter WHITE = 12'b1111_1111_1111;
+    parameter RED =   12'b1111_0000_0000;
+    parameter GREEN = 12'b0000_1111_0000;
+    parameter BLUE =  12'b0000_0000_1111;
+    parameter YELLOW = RED + GREEN;
+    parameter PURPLE = RED + BLUE;
+    parameter TEAL = GREEN + BLUE;
+    parameter ORANGE = YELLOW + RED; 
 
     always @ (SEL)
         begin
-            if (sel == 2'b00)   //blank screen
-            //fill this out, kill me XDDDDD
+            if (sel == 2'b00)   //blank screen or blue
+                R = ZEROES;
+                G = ZEROES;
+                B = BLANK ? ZEROES : ONES;
+                //R and G off, blue on unless blank
+                //fill this out, kill me XDDDDD
+
+                // if (BUTTON = 1)
+
             end
         end
 
         else if (sel == 2'b01) //horizontal lines
             begin
-                //turn on red and green for yellow
-                
+                R = BLANK ? ZEROES : ONES;  //red if not blank
+                G = BLANK ? ZEROES :
+                B = ZEROES; //this is going to make red and yellow not qquite yellow and orange
+            end
+
+        else if (sel == 2'b10) //black screen box
+            begin
+                R = BLANK ? ZEROES;
+                (verticalc > ?? && horizontalc <= ?? ) ? ONES : ZEROES;
+                G = BLANK ? ZEROES;
+                (verticalc > ?? && horizontalc <= ?? ) ? ONES : ZEROES;
+                B = BLANK ? ZEROES;
+                (verticalc > ?? && horizontalc <= ?? ) ? ONES : ZEROES;
+                //turn them on if they're in bottom right part to make white box
+
+        else if (sel == 2'b11)  //green box
+            begin
+                R = BLANK ? ZEROES;
+                (verticalc > ?? && horizontalc <= ?? ) ? ZEROES : ZEROES;
+                G = BLANK ? ZEROES;
+                (verticalc > ?? && horizontalc <= ?? ) ? ONES : ZEROES;
+                B = BLANK ? ZEROES;
+                (verticalc > ?? && horizontalc <= ?? ) ? ZEROES : ZEROES;
+                //only turn on if in range of green box ayayayaay
